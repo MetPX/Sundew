54a0fac0d99451559e5462ea810d0842ec92677c
Nicholas Lemay <nicholaslemay@sourceforge.net>
2006-05-19 20:01:06 +0000

    this is a test

--- lib/stats/FileStatsCollector.py
+++ lib/stats/FileStatsCollector.py
@@ -2,6 +2,7 @@
 MetPX Copyright (C) 2004-2006  Environment Canada
 MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
 named COPYING in the root of the source directory tree.
+added comment for test
 """
 #######################################################################################
 ##
