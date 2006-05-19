426de35b1b1c0ff2554f03cd5766527b035bfb36
psilva <peter_silva@sourceforge.net>
2006-05-19 19:52:40 +0000

    Added copyright notice.

--- lib/stats/FileStatsCollector.py
+++ lib/stats/FileStatsCollector.py
@@ -1,3 +1,8 @@
+"""
+MetPX Copyright (C) 2004-2006  Environment Canada
+MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
+named COPYING in the root of the source directory tree.
+"""
 #######################################################################################
 ##
 ## Name   : FileStatsCollector.py 
