22e175428f7aa7c4a8b789350ac2b8b423855817
Michel Grenier <michel_grenier@sourceforge.net>
2006-06-05 13:14:45 +0000

    change collection_stations.conf for stations.conf ??? Its the new file isn't it !?

--- lib/receiverAm.py
+++ lib/receiverAm.py
@@ -37,7 +37,7 @@ class receiverAm(gateway.gateway):
 
         self.logger.debug("Instanciation du bulletinManagerAm")
         
-        self.pathFichierStations = PXPaths.ETC + 'collection_stations.conf'
+        self.pathFichierStations = PXPaths.ETC + 'stations.conf'
 
         # Instanciation du bulletinManagerAm avec la panoplie d'arguments.
         self.unBulletinManager = \
