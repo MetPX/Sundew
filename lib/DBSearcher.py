ab5ba70b7bb213a564364a90cfd5262824a4faa5
Dan <dlema99@sourceforge.net>
2006-08-03 15:36:06 +0000

    Copyright added

--- lib/DBSearcher.py
+++ lib/DBSearcher.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
 """
+MetPX Copyright (C) 2004-2006  Environment Canada
+MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
+named COPYING in the root of the source directory tree.
+
 #############################################################################################
 # Name: DBSearcher.py
 #
