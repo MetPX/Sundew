dc25b2561c5539546db271e19734d7d76ed622ed
Nicholas Lemay <nicholaslemay@sourceforge.net>
2006-11-07 18:22:19 +0000

    Added --delete-before option to rsync that handles .conf files.

--- lib/stats/pickleUpdater.py
+++ lib/stats/pickleUpdater.py
@@ -384,8 +384,7 @@ def addOptions( parser ):
     
     parser.add_option( "-n", "--now", action="store_true", dest = "collectUpToNow", default=False, help="Collect data up to current second." )
        
-    parser.add_option( "-t", "--types", type="string", dest="types", default="All",
-                        help="Types of data to look for." )          
+    parser.add_option( "-t", "--types", type="string", dest="types", default="All", help="Types of data to look for." )          
 
 
 
@@ -483,10 +482,10 @@ def updateConfigurationFiles( machine, login ):
         os.makedirs(  '/apps/px/stats/trx/', mode=0777 )       
 
         
-    status, output = commands.getstatusoutput( "rsync -avzr  -e ssh %s@%s:/apps/px/etc/rx/ /apps/px/stats/rx/"  %( login, machine) ) 
+    status, output = commands.getstatusoutput( "rsync -avzr --delete-before -e ssh %s@%s:/apps/px/etc/rx/ /apps/px/stats/rx/"  %( login, machine) ) 
     #print output # for debugging only
     
-    status, output = commands.getstatusoutput( "rsync -avzr  -e ssh %s@%s:/apps/px/etc/tx/ /apps/px/stats/tx/"  %( login, machine) )  
+    status, output = commands.getstatusoutput( "rsync -avzr  --delete-before -e ssh %s@%s:/apps/px/etc/tx/ /apps/px/stats/tx/"  %( login, machine) )  
     #print output # for debugging only            
 
     
