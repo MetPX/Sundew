ddd50bd944e76594d1eedbe3ca1f8c1f7b1fb32a
Dan <dlema99@sourceforge.net>
2008-03-17 18:08:28 +0000

    1) Possibility to set a rootPath to locate the Master Configs 2) Functions to obtain information (type, cluster) about a flow have been added

--- lib/MasterConfigurator.py
+++ lib/MasterConfigurator.py
@@ -17,16 +17,25 @@ named COPYING in the root of the source directory tree.
 #############################################################################################
 
 """
-import sys, os, os.path, re, time, fnmatch
+import sys, os, os.path, commands, re, time, fnmatch
 import PXPaths
 from SystemManager import SystemManager
 from PXManager import PXManager
 
 class MasterConfigurator(object):
 
-    def __init__(self):
+    def __init__(self, rootPath=""):
+        if os.path.isdir('/users/dor/aspy/dan/data/master/'):
+            self.rootPath = '/users/dor/aspy/dan/data/master/' # developpment machine
+        elif rootPath:
+            self.rootPath = os.path.normpath(rootPath) + '/'
+        else:
+            self.rootPath = '/apps/master/'   # path under wich are the clusters and all the configs. files
+        self.types = ['source', 'client', 'sourlient']  # Possible type of flows
+        self.initAll()
+
+    def initAll(self):
         self.clusters = []                # cluster names (same as dsh)
-        self.rootPath = '/apps/master/'   # path under wich are the clusters and all the configs. files
 
         self.dupSources = []              # Duplicate sources (when you combine sources from all clusters)
         self.dupClients = []              # Duplicate clients (when you combine clients from all clusters)
@@ -42,9 +51,7 @@ class MasterConfigurator(object):
         self.clientCluster = {}           # A mapping from a client to it's cluster
         self.sourlientCluster = {}        # A mapping from a sourlient to it's cluster
         self.flowCluster = {}             # A mapping from a flow to it's cluster
-
-        self.types = ['source', 'client', 'sourlient']  # Possible type of flows
-
+        
     def printClusterInfos(self, flowCluster):
         keys = flowCluster.keys()
         keys.sort()
@@ -60,9 +67,111 @@ class MasterConfigurator(object):
     def setClusters(self, list):
         self.clusters = list
 
-    def getTypeCluster(self, flow):
+    def findClient(self, clusters=None, ip="", name=""):
+        """
+        clusters: a list of clusters (ex: ['pds', 'px', 'pxatx'])
+        ip: IP address (ex: '192.168.1.1')
+        name: hostname (ex: 'metmgr')
+
+        Only one argument in (ip, name) must be non null
+        """
+        import socket
+
+        clusters = clusters or self.clusters
+        cliClust = []
+        if ip:
+            try:
+                # get the first part of the fully qualified domain name
+                name = socket.gethostbyaddr(ip)[0].split('.')[0]
+            except:
+                pass
+
+        elif name:
+            try:
+                ip = socket.gethostbyname(name)
+            except:
+                pass
+
+        for  cluster in clusters:
+            clusterRoot = self.rootPath + cluster
+            PXPaths.normalPaths(clusterRoot)
+            if ip and name:
+                command = "grep -l -E '%s|%s' %s" % (ip, name, PXPaths.TX_CONF + "*.conf")
+            elif ip:
+                command = "grep -l -E '%s' %s" % (ip, PXPaths.TX_CONF + "*.conf")
+            elif name:
+                command = "grep -l -E '%s' %s" % (name, PXPaths.TX_CONF + "*.conf")
+
+            #print "%s" % cluster.upper()
+            output = commands.getoutput(command)
+            clients = [ (os.path.basename(cli)[:-5], cluster) for cli in output.split()]
+            cliClust.extend(clients)
+
+
+        PXPaths.normalPaths() # Reset PXPaths variables
+        return cliClust
+
+    def getTypeCluster(self, flow, init=False):
+        """
+        When init is not False, it is a cluster list
+
+        flow is the name of a client, source, sourlient
+        return a list of tuple
+        
+        getTypeCluster('aftn') => [('sourlient', 'pxatx')]
+        getTypeCluster('pds5') => [('source', 'pxatx')]
+        getTypeCluster('metmgr3') => [('client', 'pds'), ('client', 'pxatx')]
+        """
+        if init:
+            self.initAll()
+            self.clusters = init
+            self.getAllFlows()
         return self.flowCluster.get(flow, [])
 
+    def getType(self, flow, init=False):
+        """
+        When init is not False, it is a cluster list
+
+        flow is the name of a client, source, sourlient
+        return type of the flow 
+
+        getType('aftn') => 'sourlient'
+        getType('pds5') => 'source'
+        getType('metmgr3') => 'client'
+
+        """
+        if init:
+            self.initAll()
+            self.clusters = init
+            self.getAllFlows()
+        type_cluster = self.flowCluster.get(flow, [])
+        if len(type_cluster) == 1:
+            return type_cluster[0][0]
+        else:
+            return len(type_cluster)
+
+    def getCluster(self, flow, init=False):        
+        """
+        When init is not False, it is a cluster list
+
+        flow is the name of a client, source, sourlient
+        return the cluster's name  on which the flow is present
+        or the number of clusters, if more than one. 
+        
+        getCluster('aftn') => 'pxatx'
+        getCluster('pds5') => 'pxatx'
+        gettCluster('metmgr3') => 2
+        """
+        if init:
+            self.initAll()
+            self.clusters = init
+            self.getAllFlows()
+        type_cluster = self.flowCluster.get(flow, [])
+        if len(type_cluster) == 1:
+            return type_cluster[0][1]
+        else:
+            return len(type_cluster)
+            
     def createFlowDict(self):
         mergedDict = SystemManager.mergeTwoDict(self.sourceCluster, self.clientCluster)
         return SystemManager.mergeTwoDict(mergedDict, self.sourlientCluster) 
@@ -149,12 +258,32 @@ class MasterConfigurator(object):
 if __name__ == '__main__':
     mc = MasterConfigurator()
     mc.setClusters(['px', 'pds', 'pxatx'])
-    mc.getAllFlows(noPrint=False)
+    mc.getAllFlows(noPrint=True)
     print("%s: %s" % ('metmgr1', mc.getTypeCluster('metmgr1')))
+    print mc.getType('metmgr1')
+    print mc.getCluster('metmgr1')
     print("%s: %s" % ('aftn', mc.getTypeCluster('aftn')))
     print("%s: %s" % ('pds5', mc.getTypeCluster('pds5')))
+
+    print("%s: %s" % ('metmgr3', mc.getTypeCluster('metmgr3')))
+    print mc.getType('metmgr3')
+    print mc.getCluster('metmgr3')
+
+    print("%s: %s" % ('px-stage', mc.getTypeCluster('px-stage')))
+    print mc.getType('px-stage')
+    print mc.getCluster('px-stage')
+
+    print("%s: %s" % ('pds_metser', mc.getTypeCluster('pds_metser')))
+    print mc.getType('pds_metser')
+    print mc.getCluster('pds_metser')
 
     #print mc.sourceCluster
     #print mc.clientCluster
     #print mc.sourlientCluster
     #print mc.flowCluster
+
+    mc1 = MasterConfigurator()
+    print mc1.getType('metmgr1', ['px', 'pds', 'pxatx'])
+    print mc1.getCluster('metmgr1')
+
+    mc1.findClient(ip='199.212.17.60', clusters=['px', 'pxatx', 'pds'])
