4529574ab4950290613601948dd4cecf7727c2b6
Michel Grenier <michel_grenier@sourceforge.net>
2009-12-21 19:42:47 +0000

    MG  added messages for batch reached and a break per directory scan if reached

--- lib/PullFTP.py
+++ lib/PullFTP.py
@@ -334,7 +334,7 @@ class PullFTP(object):
             pdir = self.dirPattern(self.destDir)
             if pdir != '' : self.destDir = pdir
             self.destDir = self.destDir[1:]
-            self.logger.debug("cd to %s" % self.destdir )
+            self.logger.debug("cd to %s" % self.destDir )
 
             # cd to that directory
 
@@ -366,6 +366,8 @@ class PullFTP(object):
             
             filelst = self.ls.keys()
             desclst = self.ls
+
+            # improve... time sort with desclst
             filelst.sort()
 
             # if we dont delete, get file list from difference in ls
@@ -426,9 +428,14 @@ class PullFTP(object):
             # save ls file
             ok = self.write_ls_file(self.lspath)
 
+            # if reached the batch limit : break here
+            if len(files_pulled) == self.source.batch :
+               self.logger.warning("break retrieving... batch reached")
+               break
+
             # if we had a problem break here
-            if len(files_notretrieved) > 0 :
-               self.logger.warning("break retrieving... because of error or batch reached")
+            if len(files_notretrieved) > 0 or len(files_pulled) == self.source.batch :
+               self.logger.warning("break retrieving... because of error")
                break
 
         return files_pulled
@@ -525,27 +532,27 @@ class PullFTP(object):
         """
 
         if keywd[:10] == "{YYYYMMDD}"    : 
-                                           return   time.strftime("%Y%m%d", time.gmtime())
+                                           return   time.strftime("%Y%m%d", time.gmtime()) + keywd[10:]
 
         if keywd[:13] == "{YYYYMMDD-1D}" :
                                            epoch  = time.mktime(time.gmtime()) - 24*60*60
-                                           return   time.strftime("%Y%m%d", time.localtime(epoch)  )
+                                           return   time.strftime("%Y%m%d", time.localtime(epoch) ) + keywd[13:]
 
         if keywd[:13] == "{YYYYMMDD-2D}" :
                                            epoch  = time.mktime(time.gmtime()) - 48*60*60
-                                           return   time.strftime("%Y%m%d", time.localtime(epoch)  )
+                                           return   time.strftime("%Y%m%d", time.localtime(epoch) ) + keywd[13:]
 
         if keywd[:13] == "{YYYYMMDD-3D}" :
                                            epoch  = time.mktime(time.gmtime()) - 72*60*60
-                                           return   time.strftime("%Y%m%d", time.localtime(epoch)  )
+                                           return   time.strftime("%Y%m%d", time.localtime(epoch) ) + keywd[13:]
 
         if keywd[:13] == "{YYYYMMDD-4D}" :
                                            epoch  = time.mktime(time.gmtime()) - 96*60*60
-                                           return   time.strftime("%Y%m%d", time.localtime(epoch)  )
+                                           return   time.strftime("%Y%m%d", time.localtime(epoch) ) + keywd[13:]
 
         if keywd[:13] == "{YYYYMMDD-5D}" : 
                                            epoch  = time.mktime(time.gmtime()) - 120*60*60
-                                           return   time.strftime("%Y%m%d", time.localtime(epoch)  )
+                                           return   time.strftime("%Y%m%d", time.localtime(epoch) ) + keywd[13:]
 
         return defval
 
