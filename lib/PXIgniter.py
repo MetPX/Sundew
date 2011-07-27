9214c6dd28fbfb8b8730fd633c49c7579d3b4747
Michel Grenier <michel_grenier@sourceforge.net>
2011-07-27 19:31:30 +0000

    MG  no more protocol checking

--- lib/PXIgniter.py
+++ lib/PXIgniter.py
@@ -78,14 +78,12 @@ class PXIgniter(Igniter):
          if not commands.getstatusoutput('ps -p ' + str(self.lockpid))[0]:
             os.unlink(self.lock)
             os.kill(self.lockpid, signal.SIGTERM)
-            # sftp/ftp are not always responsive to sigterm sometimes... force sigkill after .5 sec
+            # if a sender is responsive to sigterm... after .5 sec force sigkill 
             if self.direction == 'sender' :
-               client = Client(self.flowName, self.logger)
-               if client.protocol[-3:] == 'ftp' :
-                  time.sleep(0.5)
-                  if not commands.getstatusoutput('ps -p ' + str(self.lockpid))[0]:
-                     self.logger.info("%s %s (sigkill) has been stopped" % (self.direction.capitalize(), self.flowName))
-                     os.kill(self.lockpid, signal.SIGKILL)
+               time.sleep(0.5)
+               if not commands.getstatusoutput('ps -p ' + str(self.lockpid))[0]:
+                  self.logger.info("%s %s (sigkill) has been stopped" % (self.direction.capitalize(), self.flowName))
+                  os.kill(self.lockpid, signal.SIGKILL)
 
          # ... and not running
          else:
