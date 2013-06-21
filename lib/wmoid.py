4a620b9828cf3f9cfde6b14abd0d3e0e48c72c05
Michel Grenier <michel_grenier@sourceforge.net>
2013-06-21 13:11:33 +0000

    MG Python3 compatible

--- lib/wmoid.py
+++ lib/wmoid.py
@@ -9,6 +9,8 @@
 # Description: parse the wmo_id.conf file
 #              this file is used to force the station name into the ingested filename
 #
+# MG python3 compatible
+#
 #############################################################################################
 """
 
@@ -51,4 +53,4 @@ class wmoid:
       
 if __name__ == '__main__':
 
-    print wmoid(None).parse()
+    print(wmoid(None).parse())
