d3fe52dcfd86391cb23e46af5cf74e503fff4840
michel grenier <michel.grenier2@canada.ca>
2015-12-02 17:51:19 -0500

    MG  do_nothing.py fx_script example

---
+++ etc/scripts/do_nothing.py
@@ -0,0 +1,21 @@
+import commands, sys, os, os.path, time, stat, string
+##
+# version  1.0 Michel Grenier Jan 2007
+#
+# cvt_togif.py
+#              Ce fx_script convertit les fichiers en format GIF
+##
+
+class Transformer(object):
+
+      def perform(self, ipath, logger ):
+
+          return None
+
+transformer=Transformer()
+
+# fx_script setting
+self.fx_script=transformer.perform
+
+# manual testing
+#transformer.perform(sys.argv[1],None)
